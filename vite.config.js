419fc34f8f1c062b40cbd5db65d524cd2c3ad115
Gustavo dos Santos <Guga_Vine@outlook.com>
2024-07-05 22:28:10 -0300

    Adicionando react-router-dom e fazendo a rota inicial

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path"
+import { resolve } from 'path';
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
-// https://vitejs.dev/config/
+const BASE = (process.env.VITE_API_URL ?? '/');
+
+
 export default defineConfig({
+  base: BASE,
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
