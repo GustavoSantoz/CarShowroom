686368d80bb5560c7457fe3e8260e75051f24bdf
Gustavo dos Santos <Guga_Vine@outlook.com>
2024-07-05 22:36:15 -0300

    Configurando o deploy e mudando a rota

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,7 @@ export default function Login() {
     return (
         <div className="flex flex-col items-center justify-center w-screen h-screen bg-gradient-to-b from-blue-500 to-purple-500">
             <h1 className="text-white text-4xl font-bold">Car Showroom</h1>
-            <button className="bg-white text-black rounded-lg px-4 py-2 font-bold">Login</button>
+            <button className="bg-white text-black rounded-lg px-4 py-2 font-bold" onClick={() => window.location.href = '/home'}>Login</button>
         </div>
     )
 }
