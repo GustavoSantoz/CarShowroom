aae052331daf0b46559b50496e3da65d309a04b3
Gustavo dos Santos <Guga_Vine@outlook.com>
2024-07-05 23:05:25 -0300

    Criando a página home

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
-import Login from './components/Login'
 import { Route, Routes } from 'react-router-dom'
+import Login from './components/Login'
+import Home from './pages/home'
 
 function App() {
   return (
     <>
        <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Login />} />
+        <Route path="/home" element={<Home />} />
         <Route path="*" element={<Login />} />
       </Routes>
     </>
