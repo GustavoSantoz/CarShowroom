43fd2f422d7113c545cd76027c5e5cc48beef39d
Gustavo dos Santos <Guga_Vine@outlook.com>
2024-07-06 00:37:35 -0300

    Alterando o header e movendo o Login de pasta

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 export default function Login() {
     return (
-        <div className="flex flex-col items-center justify-center w-screen h-screen">
-            <h1 className="text-white text-4xl font-bold mb-5">Car Showroom</h1>
+        <div className="flex flex-col items-center justify-center w-screen h-screen bg-[#141414]">
+            <h1 className="text-white text-4xl font-bold mb-5 md:text-md">Car Showroom</h1>
             <button className="bg-white text-black rounded-lg px-4 py-2 font-bold" onClick={() => window.location.href = '/home'}>Login</button>
         </div>
     )
